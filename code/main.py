f8c3ddfd460a2d36458ebcb0c28867a39bd4ced5
Lucky-Graduated <luckygraduated@gmail.com>
2024-06-06 19:00:34 +0530

    changes

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 from settings import *
 from sprites import *
 from groups import AllSprites
+from support import *
 
 class Game:
     def __init__(self):
@@ -14,7 +15,16 @@ class Game:
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
+        self.load_assets()
         self.setup()
+
+    def load_assets(self):
+        # graphics 
+        self.player_frames = import_folder('images', 'player')
+        self.bullet_surf = import_image('images', 'gun', 'bullet')
+        self.fire_surf = import_image('images', 'gun', 'fire')
+        self.bee_frames = import_folder('images', 'enemies', 'bee')
+        self.worm_frames = import_folder('images', 'enemies', 'worm')
 
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
@@ -27,7 +37,10 @@ class Game:
         
         for obj in map.get_layer_by_name('Entities'):
             if obj.name =='Player':
-                self.player = Player((obj.x, obj.y),self.all_sprites,self.collision_sprites)
+                self.player = Player((obj.x, obj.y),self.all_sprites,self.collision_sprites, self.player_frames)
+        
+        Bee(self.bee_frames, (500, 600), (self.all_sprites, self.collision_sprites))
+        Worm(self.worm_frames, (200, 600), (self.all_sprites, self.collision_sprites))
 
 
     def run(self):
--- code/sprites.py
+++ code/sprites.py
@@ -6,52 +6,85 @@ class Sprite(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos,groups, collision_sprites):
-        super().__init__(groups)
-        self.image = pygame.image.load(join('images', 'player', '0.png')).convert_alpha()
-        self.rect = self.image.get_frect(center = pos)
+class AnimatedSprite(Sprite):
+    def __init__(self, frames, pos, groups):
+        self.frames, self.frame_index, self.animation_speed = frames, 0, 10
+        super().__init__(pos, self.frames[self.frame_index], groups)
 
-        # movement and collisions
-        self.speed = 400
-        self.direction = pygame.math.Vector2()
-        self.collision_sprites = collision_sprites
-        self.gravity = 50
-        self.is_onfloor = False
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+
+class Player(AnimatedSprite):
+    def __init__(self, pos, groups, collision_sprites, frames):
+        super().__init__(frames, pos, groups)
+        self.flip = False
     
+        # movement & collision
+        self.direction = pygame.Vector2()
+        self.collision_sprites = collision_sprites
+        self.speed = 400
+        self.gravity = 50
+        self.on_floor = False
+
     def input(self):
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        if keys[pygame.K_w] and self.is_onfloor:
+        self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        if keys[pygame.K_SPACE] or keys[pygame.K_w] and self.on_floor:
             self.direction.y = -20
 
     def move(self, dt):
         # horizontal
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
-
-        # vertical
-        self.is_onfloor = False
+        
+        # vertical 
         self.direction.y += self.gravity * dt
         self.rect.y += self.direction.y
         self.collision('vertical')
-    
+
     def collision(self, direction):
         for sprite in self.collision_sprites:
             if sprite.rect.colliderect(self.rect):
-                if direction =='horizontal':
+                if direction == 'horizontal':
                     if self.direction.x > 0: self.rect.right = sprite.rect.left
                     if self.direction.x < 0: self.rect.left = sprite.rect.right
-                if direction =='vertical':
+                if direction == 'vertical':
                     if self.direction.y > 0: self.rect.bottom = sprite.rect.top
                     if self.direction.y < 0: self.rect.top = sprite.rect.bottom
                     self.direction.y = 0
 
     def check_floor(self):
-        bottom_rect = pygame.FRect((0, 0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
-        self.is_onfloor = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 else False
-    
+        bottom_rect = pygame.FRect((0,0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
+        self.on_floor = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 else False
+
+    def animate(self, dt):
+        if self.direction.x:
+            self.frame_index += self.animation_speed * dt
+            self.flip = self.direction.x < 0
+        else:
+            self.frame_index = 0
+
+        self.frame_index = 1 if not self.on_floor else self.frame_index
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+        self.image = pygame.transform.flip(self.image, self.flip, False)
+
     def update(self, dt):
         self.check_floor()
         self.input()
         self.move(dt)
+        self.animate(dt)
+
+class Bee(AnimatedSprite):
+    def __init__(self, frames, pos, groups):
+        super().__init__(frames, pos, groups)
+    
+    def update(self, dt):
+        self.animate(dt)
+
+class Worm(AnimatedSprite):
+    def __init__(self, frames, pos, groups):
+        super().__init__(frames, pos, groups)
+    
+    def update(self, dt):
+        self.animate(dt)
